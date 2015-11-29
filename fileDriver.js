6bf1225c6dee099793443fe93b1ff0b087b9f0aa
CBUU <ioricbuu@gmail.com>
2015-11-29 17:14:56 +0800

    first ci

---
+++ fileDriver.js
@@ -0,0 +1,92 @@
+
+var ObjectID = require('mongodb').ObjectID,
+	fs = require('fs');
+
+FileDriver = function(db){
+	this.db = db;
+};
+
+
+FileDriver.prototype.getCollection = function(callback) {
+	this.db.collection('files', function(error, file_collection) { //1
+		if( error ) callback(error);
+		else callback(null, file_collection);
+	});
+};
+
+FileDriver.prototype.get = function(id, callback) {
+	this.getCollection(function(error, file_collection) { //1
+		if (error) callback(error);
+		else {
+			var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$"); //2
+			if (!checkForHexRegExp.test(id)) callback({error: "invalid id"});
+			else file_collection.findOne({'_id':ObjectID(id)}, function(error,doc) { //3
+				if (error) callback(error);
+				else callback(null, doc);
+			});
+		}
+	});
+};
+
+FileDriver.prototype.handleGet = function(req, res) { //1
+	var fileId = req.params.id;
+	if (fileId) {
+		this.get(fileId, function (error, thisFile) { //2
+			if (error) {
+				res.status(400).send(error);
+			}
+			else {
+				if (thisFile) {
+					var filename = fileId + thisFile.ext; //3
+					var filePath = __dirname + '/uploads/' + filename; //4
+					res.sendFile(filePath); //5
+				} else res.status(404).send('file not found');
+			}
+		});
+	} else {
+		res.status(404).send('file not found');
+	}
+};
+
+FileDriver.prototype.save = function(obj, callback) { //1
+	this.getCollection(function(error, the_collection) {
+		if( error ) callback(error);
+		else {
+			obj.created_at = new Date();
+			the_collection.insert(obj, function() {
+				callback(null, obj);
+			});
+		}
+	});
+};
+
+FileDriver.prototype.getNewFileId = function(newobj, callback) { //2
+	this.save(newobj, function(err,obj) {
+		if (err) { callback(err); }
+		else { callback(null,obj._id); } //3
+	});
+};
+
+FileDriver.prototype.handleUploadRequest = function(req, res) { //1
+	var ctype = req.get("content-type"); //2
+	var ext = ctype.substr(ctype.indexOf('/')+1); //3
+	if (ext) {ext = '.' + ext; } else {ext = '';}
+	this.getNewFileId({'content-type':ctype, 'ext':ext}, function(err,id) { //4
+		if (err) { res.status(400).send(err); }
+		else {
+			var filename = id + ext; //5
+			filePath = __dirname + '/uploads/' + filename; //6
+
+			var writable = fs.createWriteStream(filePath); //7
+			req.pipe(writable); //8
+			req.on('end', function (){ //9
+				res.status(201).send({'_id':id});
+			});
+			writable.on('error', function(err) { //10
+				res.status(500).send(err);
+			});
+		}
+	});
+};
+
+exports.FileDriver = FileDriver;
---
+++ index.js
@@ -0,0 +1,132 @@
+var http = require('http'),
+    express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    FileDriver = require('./fileDriver').FileDriver,
+    CollectionDriver = require('./collectionDriver').CollectionDriver;
+
+var app = express();
+app.set('port',process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+var mongoHost = 'localHost'; //A
+var mongoPort = 27017;
+var collectionDriver;
+var fileDriver;
+
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort));
+mongoClient.open(function(err, mongoClient) { //C
+    if (!mongoClient) {
+        console.error("Error! Exiting... Must start MongoDB first");
+        process.exit(1); //D
+    }
+    var db = mongoClient.db("test");  //E
+    collectionDriver = new CollectionDriver(db); //F
+    fileDriver = new FileDriver(db);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function (req, res) {
+    res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+
+app.post('/files', function(req,res) {fileDriver.handleUploadRequest(req,res);});
+
+app.get('/files/:id', function(req, res) {fileDriver.handleGet(req,res);});
+
+app.get('/:collection', function(req, res, next) {
+    var params = req.params;
+    var query = req.query.query; //1
+    if (query) {
+        query = JSON.parse(query); //2
+        collectionDriver.query(req.params.collection, query, returnCollectionResults(req,res)); //3
+    } else {
+        collectionDriver.findAll(req.params.collection, returnCollectionResults(req,res)); //4
+    }
+});
+
+function returnCollectionResults(req, res) {
+    return function(error, objs) { //5
+        if (error) { res.send(400, error); }
+        else {
+            if (req.accepts('html')) { //6
+                res.render('data',{objects: objs, collection: req.params.collection});
+            } else {
+                res.set('Content-Type','application/json');
+                res.send(200, objs);
+            }
+        }
+    };
+};
+
+app.get('/:collection/:entity', function(req, res) { //I
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+    if (entity) {
+        collectionDriver.get(collection, entity, function(error, objs) { //J
+            if (error) { res.status(400).send(err); }
+            else { res.status(200).send(objs); } //K
+        });
+    } else {
+        res.send(400, {error: 'bad url', url: req.url});
+    }
+});
+
+app.post('/:collection', function(req, res) { //A
+    var object = req.body;
+    var collection = req.params.collection;
+    console.log(collection);
+    collectionDriver.save(collection, object, function(err,docs) {
+        if (err) { res.status(400).send(err); }
+        else { res.status(201).send(docs);  console.log(docs);} //B
+    });
+});
+
+app.put('/:collection/:entity', function(req, res) { //A
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+    if (entity) {
+        collectionDriver.update(collection, req.body, entity, function(error, objs) { //B
+            if (error) { res.status(400).send(err); }
+            else { res.status(200).send(objs); } //C
+        });
+    } else {
+        var error = { "message" : "Cannot PUT a whole collection" };
+        res.status(400).send(err);
+    }
+});
+
+
+app.delete('/:collection/:entity', function(req, res) { //A
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+    if (entity) {
+        collectionDriver.delete(collection, entity, function(error, objs) { //B
+            if (error) { res.status(400).send(err); }
+            else { res.status(200).send(objs); } //C 200 b/c includes the original doc
+        });
+    } else {
+        var error = { "message" : "Cannot DELETE a whole collection" };
+        res.status(400).send(err);
+    }
+});
+
+
+
+// error handle for 404 not found
+app.use(function (req,res) {
+    res.render('404', {url:req.url});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
